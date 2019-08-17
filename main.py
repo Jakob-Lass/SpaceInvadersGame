6f621a6554af3a5313b552b0bcfb8c5512b0ace5
Jakob Lass <lass.jakob@gmail.com>
2019-08-17 15:01:01 +0200

    Add movement to left and right plus boundary check

--- main.py
+++ main.py
@@ -3,9 +3,18 @@
 # Space invaders
 
 import turtle
+
+turtle.fd(0)
+turtle.speed(0) # maximize drawing speed
+turtle.ht() # Hide turtle 
+turtle.setundobuffer(1)
+turtle.tracer(1)
 import os
 
 # Set up the main window
+# Screen size
+screenWidth = 600
+screenHight = 600
 
 wn = turtle.Screen()
 wn.bgcolor('black')
@@ -35,8 +44,21 @@ player.speed(0)
 player.setheading(90) # rotate 90 degrees
 
 player.setposition(0,-250)
+player.width = 30
+
+playerspeed = 15
+# Move player left and right
+
+def moveLeft(player,screenWidth):
+    player.setx(*[player.xcor()-playerspeed if player.xcor() >-screenWidth/2.0+player.width/2.0 else player.xcor()])
+
+def moveRight(player,screenWidth):
+    player.setx(*[player.xcor()+playerspeed if player.xcor() <screenWidth/2.0-player.width/2.0 else player.xcor()])
+
+# Keyboard binding
+turtle.listen()
+turtle.onkey(lambda:moveLeft(player,screenWidth), 'a')
+turtle.onkey(lambda:moveRight(player,screenWidth), 'd')
 
 
-
-
-delay = input("Press any key to finish")
+delay = input("Press enter key to finish")
