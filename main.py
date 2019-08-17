4715ae7030153d54a74d488c067f3975884956e9
Jakob Lass <lass.jakob@gmail.com>
2019-08-17 14:45:24 +0200

    Add border and player

--- main.py
+++ main.py
@@ -12,8 +12,29 @@ wn.bgcolor('black')
 
 wn.title('Space Invaders')
 
+# Draw border around window
+border_pen = turtle.Turtle()
+border_pen.speed(0)
+border_pen.color('white')
+border_pen.penup()
+border_pen.setposition(-300,-300)
+border_pen.pendown()
+border_pen.pensize(3)
+for side in range(4):
+    border_pen.fd(600)
+    border_pen.lt(90)
 
+border_pen.hideturtle()
 
+# Create player
+player = turtle.Turtle()
+player.color('blue')
+player.shape('triangle')
+player.penup()
+player.speed(0)
+player.setheading(90) # rotate 90 degrees
+
+player.setposition(0,-250)
 
 
 
