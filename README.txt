71e7b335b130a743ee976e6a0ed39db11c542e51
Jakob Lass <lass.jakob@gmail.com>
2019-08-17 14:34:13 +0200

    Initial commit

---
+++ README.txt
@@ -0,0 +1,4 @@
+Small program to test out plotting features and GUI for a python game.
+
+Following the tutorial of https://www.youtube.com/watch?v=crV6T3piwHQ
+
---
+++ main.py
@@ -0,0 +1,21 @@
+#!usr/bin/env python3
+
+# Space invaders
+
+import turtle
+import os
+
+# Set up the main window
+
+wn = turtle.Screen()
+wn.bgcolor('black')
+
+wn.title('Space Invaders')
+
+
+
+
+
+
+
+delay = input("Press any key to finish")
