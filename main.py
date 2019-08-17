545d6ae831d915daa1799a808f815e4a438d1141
Jakob Lass <lass.jakob@gmail.com>
2019-08-17 15:35:51 +0200

    Add bullet and collission check If enemy reaches bottom game is over

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Space invaders
 
 import turtle
-
+import math
 turtle.fd(0)
 turtle.speed(0) # maximize drawing speed
 turtle.ht() # Hide turtle 
@@ -14,7 +14,7 @@ import os
 # Set up the main window
 # Screen size
 screenWidth = 600
-screenHight = 600
+screenHeight = 600
 
 wn = turtle.Screen()
 wn.bgcolor('black')
@@ -46,19 +46,94 @@ player.setheading(90) # rotate 90 degrees
 player.setposition(0,-250)
 player.width = 30
 
-playerspeed = 15
+playerSpeed = 15
+
 # Move player left and right
 
-def moveLeft(player,screenWidth):
-    player.setx(*[player.xcor()-playerspeed if player.xcor() >-screenWidth/2.0+player.width/2.0 else player.xcor()])
+def moveLeft(player,screenWidth,playerSpeed):
+    player.setx(*[player.xcor()-playerSpeed if player.xcor() >-screenWidth/2.0+player.width/2.0 else player.xcor()])
 
-def moveRight(player,screenWidth):
-    player.setx(*[player.xcor()+playerspeed if player.xcor() <screenWidth/2.0-player.width/2.0 else player.xcor()])
+def moveRight(player,screenWidth,playerSpeed):
+    player.setx(*[player.xcor()+playerSpeed if player.xcor() <screenWidth/2.0-player.width/2.0 else player.xcor()])
+
+# Create player weapon
+
+bullet = turtle.Turtle()
+bullet.color('yellow')
+bullet.shape('triangle')
+bullet.penup()
+bullet.speed(0)
+bullet.setheading(90)
+bullet.shapesize(0.5,0.5)
+bullet.hideturtle()
+bullet.state = 'ready' # 'ready' to fire or 'fire'
+bulletSpeed = -25
+
+def fireBullet(player,bulletSpeed,bullet):
+    if bullet.state == 'ready':
+        bullet.setx(player.xcor())
+        bullet.sety(player.ycor())
+        bullet.state = 'fire'
+        bullet.showturtle()
+    
+
+
+# Create enemy
+
+enemy = turtle.Turtle()
+enemy.color('red')
+enemy.shape('circle')
+enemy.penup()
+enemy.speed(0)
+enemy.setposition(-200, -250)
+
+enemy.width = 15
+
+enemySpeed = 2
 
 # Keyboard binding
 turtle.listen()
-turtle.onkey(lambda:moveLeft(player,screenWidth), 'a')
-turtle.onkey(lambda:moveRight(player,screenWidth), 'd')
+turtle.onkey(lambda:moveLeft(player,screenWidth,playerSpeed), 'a')
+turtle.onkey(lambda:moveLeft(player,screenWidth,playerSpeed), 'Left')
+turtle.onkey(lambda:moveRight(player,screenWidth,playerSpeed), 'd')
+turtle.onkey(lambda:moveRight(player,screenWidth,playerSpeed), 'Right')
+turtle.onkey(lambda:fireBullet(player,bulletSpeed,bullet), 'space')
+
+def collission(obj1, obj2, minDistance = 1):
+    vectorBetween = [obj1.xcor()-obj2.xcor(),obj1.ycor()-obj2.ycor()]
+    distance = math.sqrt(vectorBetween[0]**2+vectorBetween[1]**2)
+    return distance<minDistance
+
+# main Game loop 
+
+while True:
+    #Move enemy 
+    x = enemy.xcor()
+    x += enemySpeed
+    if x > screenWidth/2.0-player.width/2.0 or x <-screenWidth/2.0+enemy.width/2.0:
+        enemySpeed*=-1
+        y = enemy.ycor()
+        y -= 40
+        enemy.sety(y)
+        if y<-screenHeight/2.0+enemy.width/2.0:
+            print('Game OVER!')
+            break
+    enemy.setx(x)
+
+    if bullet.state == 'fire':
+        y = bullet.ycor()-bulletSpeed
+        if y>screenHight/2.0:
+            bullet.state = 'ready'
+            bullet.hideturtle()
+        bullet.sety(y)
+
+        if collission(bullet,enemy,minDistance=15):
+            bullet.state = 'ready'
+            bullet.hideturtle()
+            enemy.hideturtle()
+        
+
+
 
 
 delay = input("Press enter key to finish")
